40f4ede2a66ba7879e5573e5dda18dee3dcacef0
Yuyama-Syo <yuyamasyou@icloud.com>
2022-07-19 14:47:01 +0900

    08-1

--- app/Post.php
+++ app/Post.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App;
 
-use Illuminate\Http\Request;
+use Illuminate\Database\Eloquent\Model;
 
-class PostController extends Controller
+class Post extends Model
 {
     //
 }
