40db184a167c30e65cc83e0ca4bca9e70247042e
Yuyama-Syo <yuyamasyou@icloud.com>
2022-08-10 17:50:42 +0900

    9-1

--- database/migrations/2022_08_10_151756_add_image_column_to_posts_table.php
+++ database/migrations/2022_08_10_151756_add_image_column_to_posts_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePostsTable extends Migration
+class AddImageColumnToPostsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,7 @@ class CreatePostsTable extends Migration
      */
     public function up()
     {
-        Schema::create('posts', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('title', 50);
-            $table->string('body', 200);
-            $table->timestamps();
-            $table->softDeletes();
+        Schema::table('posts', function (Blueprint $table) {
             $table->string('image', 100)->nullable();
         });
     }
@@ -30,6 +25,8 @@ class CreatePostsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('posts');
+        Schema::table('posts', function (Blueprint $table) {
+            //
+        });
     }
 }
